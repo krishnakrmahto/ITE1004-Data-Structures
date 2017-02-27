15c9fbf0a5937c1970c70cf9a05d1d5405b25eeb
Krishna Kumar Mahto <krishnakumar.mahto2016@vitstudent.ac.in>
2017-02-27 14:38:26 +0530

    Add files via upload

--- hanoi_without_recursion.c
+++ hanoi_without_recursion.c
@@ -18,7 +18,7 @@ int pop1()
 }
 void push2(int item)
 {
-	st1.a[++st2.top]=item;
+	st2.a[++st2.top]=item;
 }
 int pop2()
 {
@@ -27,7 +27,7 @@ int pop2()
 }
 void push3(int item)
 {
-	st1.a[++st3.top]=item;
+	st3.a[++st3.top]=item;
 }
 int pop3()
 {
@@ -36,52 +36,63 @@ int pop3()
 }
 int main()
 {
+	char source='S',dest='D',aux='A';
 	st1.top=-1;st2.top=-1;st3.top=-1;
 	int n,i,moves;
 	puts("Enter the number of discs: ");
 	scanf("%d",&n);
 	moves=(pow(2,n)-1);
+	if(n%2==0)
+	{
+		dest='A';
+		aux='D';
+	}	
 	for(i=n;i>=1;i--)
 	push1(i);//first stack is filled as n,n-1,n-2,......,3,2,1
 	for(i=1;i<=moves;i++)//each oop iteration stands for one move
 	{
 		if((i%3)==1) //condtion for source to destination and vice-versa
 		{
-			if((st1.a[st1.top]<st3.a[st3.top])||(st3.top==-1))
+			if(st1.a[st1.top]<st3.a[st3.top] || st3.top==-1 && st1.top!=-1)
 			{
 				push3(pop1());
-				puts("Move from S to D.");
+				printf("Move from %c to %c\n",source,dest);
 			}
 			else
 			{
 				push1(pop3());
-				puts("Move from D to S.");
+				printf("Move from%c to %c\n",dest,source);
+				//puts("Move from D to S.");
 			}
 		}
 		else if((i%3)==0)//condition for dest to aux and vice versa
 		{
-			if(st2.a[st2.top]<st3.a[st3.top] || st3.top==-1)
+			if(st2.a[st2.top]<st3.a[st3.top] || st3.top==-1 && st2.top!=-1)
 			{
 				push3(pop2());
-				puts("Move from A to D.");
+				printf("Move from %c to %c\n",aux,dest);
+				//puts("Move from A to D.");
 			}
 			else
 			{
 				push2(pop3());
-				puts("Move from D to A.");
+				printf("Move from %c to %c\n",dest,aux);
+				//puts("Move from D to A.");
 			}
 		}
 		else if((i%3)==2)
 		{
-			if(st2.a[st2.top]<st1.a[st1.top])
+			if((st1.a[st1.top]<st2.a[st2.top]|| st2.top==-1) && st1.top!=-1)
 			{
-				push1(pop2());
-				puts("Move from S to A.");
+				push2(pop1());
+				printf("Move from %c to %c\n",source,aux);
+				//puts("Move from S to A.");
 			}
 			else
 			{
-				push2(pop1());
-				puts("Move from A to S.");
+				push1(pop2());
+				printf("Move from %c to %c\n",aux,source);
+				//puts("Move from A to S.");
 			}
 		}
 	}
