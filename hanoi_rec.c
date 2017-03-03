a4846523139d47f62f0f5d6cf17dadbef47b7845
Krishna Kumar Mahto <krishnakumar.mahto2016@vitstudent.ac.in>
2017-03-03 23:08:24 +0530

    Add files via upload
    towers of hanoi using recursive algorithm.

---
+++ hanoi_rec.c
@@ -0,0 +1,20 @@
+void tower(int,char,char,char);
+int main()
+{
+	char source='S',tar='T',aux='A';
+	int n;
+	puts("Enter the number of disks: ");
+	scanf("%d",&n);
+	hanoi(n,source,tar,aux);
+}
+void hanoi(n,source,tar,aux)
+{
+	if(n==1)
+	printf("Move %d disc from %c to %c\n",n,source,tar);
+	else
+	{
+		hanoi(n-1,source,aux,tar);
+		printf("Move %d disc from %c to %c\n",n,source,tar);
+		hanoi(n-1,aux,tar,source);
+	}
+}
