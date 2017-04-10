0a35375e621f84a3123410c76be68d91168a10b7
Krishna Kumar Mahto <krishnakumar.mahto2016@vitstudent.ac.in>
2017-04-10 23:21:56 +0530

    Add files via upload
    medium level completed!!

--- task11.c
+++ task11.c
@@ -6,16 +6,23 @@ typedef struct node
 	struct node *next;
 }FLAMES_NODE;
 FLAMES_NODE *head,*tail,*nw;
-int matching_chars(char *name1,char *name2)
+int unmatching_chars(char *name1,char *name2)
 {
-	int count;
-	for(;name1!='\0';name1++)
-	for(;name2!='\0';name2++)
+    char *temp;
+	int count=0,unmatched=0;
+	for(;*name1!='\0';name1++)
 	{
-		if(*name1==*name2);
-		count++;
+		for(temp=name2;*temp!='\0';temp++)
+		{
+			if(*name1==*temp)
+			break;
+			count++;
+		}
+		if(count==strlen(name2))
+		unmatched++;
+		count=0;
 	}
-	return(count);
+	return(unmatched);
 }
 void delete_char(FLAMES_NODE* del)
 {
@@ -23,6 +30,7 @@ void delete_char(FLAMES_NODE* del)
 	if(del==head)
 	{
 		head=head->next;
+		tail->next=head;
 		del->next=NULL;
 		free(del);
 	}
@@ -32,7 +40,7 @@ void delete_char(FLAMES_NODE* del)
 		continue;
 		tail=temp;
 		temp=temp->next;
-		tail->next=NULL;
+		tail->next=head;
 		free(temp);
 	}
 	else
@@ -44,23 +52,25 @@ void delete_char(FLAMES_NODE* del)
 		free(del);
 	}
 }
-char play(n)
+char play(int n,FLAMES_NODE *start)
 {
-	FLAMES_NODE *temp;
+    FLAMES_NODE *next_start;
+	FLAMES_NODE *temp=start;
 	int i;
-	if(n!=1)
+	if(head!=tail)
 	{
-		for(i=0;i<n;i++)
+		for(i=0;i<n-1;i++)
 		temp=temp->next;
+		next_start=temp->next;
 		delete_char(temp);
-		play(n);
+		play(n,next_start);
 	}
 	return;
 }
 int main()
 {
-	int i,choice,match_num;
-	char flames[]="FLAMES",*name1,*name2;
+	int i,choice,unmatch_num;
+	char flames[]="FLAMES",name[2][30];
 	nw=(FLAMES_NODE*)malloc(sizeof(FLAMES_NODE));
 	nw->alpha=flames[0];
 	head=nw;
@@ -72,13 +82,35 @@ int main()
 		nw->alpha=flames[i];
 		tail->next=nw;
 		tail=nw;
-		tail->next=NULL;
+		tail->next=head;
 	}
 	puts("enter the two names: ");
-	scanf("%s",name1);
-	scanf("%s",name2);
-	match_num=matching_chars(name1,name2);
-	play(match_num);
-	printf("The leter let is: %c\n",head->alpha);
-	return 0;
+	scanf("%s",name[0]);
+	scanf("%s",name[1]);
+	unmatch_num=unmatching_chars(name[0],name[1]);
+	unmatch_num+=unmatching_chars(name[1],name[0]);
+	play(unmatch_num,head);
+	printf("Letter left: %c\n",head->alpha);
+	switch(head->alpha)
+	{
+		case 'F':
+			puts("F for Friends!");
+			break;
+		case 'L':
+			puts("L for Love!");
+			break;
+		case 'A':
+			puts("A for Affection!");
+			break;
+		case 'M':
+			puts("M for Marriage!");
+			break;
+		case 'E':
+			puts("E for Enmity!");
+			break;
+		case 'S':
+			puts("S for Sister!");
+			break;
+	}
 }
+
