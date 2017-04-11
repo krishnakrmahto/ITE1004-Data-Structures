a6a5a1d5217330b90eb674f60c1eb577846c8365
Krishna Kumar Mahto <krishnakumar.mahto2016@vitstudent.ac.in>
2017-04-11 12:53:30 +0530

    Add files via upload
    Searching for roll numbers that is not in the records is causing the output console crash.

--- task10.c
+++ task10.c
@@ -1,23 +1,28 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define SIZE 10
+//one node for one student
 typedef struct record
 {
-	char *names;
+	char name[30];
 	int roll;
 }RECORD;
-typedef struct node 
+typedef struct node
 {
 	RECORD  st_rec;
 	struct node *prev,*next;
 }NODE;
 NODE *head,*tail,*nw;
+int count=1;
 void create_rec(int n)
 {
-	int i,count=1;
+	int i;
 	nw=(NODE*)malloc(sizeof(NODE));
-	printf("Student %d\nenter roll number: ",count++);
+	printf("Student %d\nenter roll number:\n",count++);
 	scanf("%d",&nw->st_rec.roll);
 	puts("enter name: ");
-	scanf("%s",nw->st_rec.names);
+	scanf("%s",nw->st_rec.name);
 	puts("");
 	head=nw;
 	head->prev=NULL;
@@ -29,7 +34,7 @@ void create_rec(int n)
 		printf("Student %d\nenter roll number: \n",count++);
 		scanf("%d",&nw->st_rec.roll);
 		puts("enter name: ");
-		scanf("%s",nw->st_rec.names);
+		scanf("%s",nw->st_rec.name);
 		tail->next=nw;
 		nw->prev=tail;
 		tail=nw;
@@ -39,19 +44,43 @@ void create_rec(int n)
 }
 void disp_pn(NODE *address)
 {
-	printf("Previous record: %d- %s\n",address->prev->st_rec.roll,address->prev->st_rec.names);
-	printf("Next record: %d- %s\n\n",address->next->st_rec.roll,address->next->st_rec.names);
+	if(address!=head)
+	printf("Previous record: %d- %s\n",address->prev->st_rec.roll,address->prev->st_rec.name);
+	else
+	puts("It's the first element.");
+	if(address!=tail)
+	printf("Next record: %d- %s\n\n",address->next->st_rec.roll,address->next->st_rec.name);
+	else
+	puts("It's the last element.'");
+}
+void insert_rec()
+{
+	nw=(NODE*)malloc(sizeof(NODE));
+	printf("Student %d\nenter roll number: \n",count++);
+	scanf("%d",&nw->st_rec.roll);
+	puts("enter name: ");
+	scanf("%s",nw->st_rec.name);
+	tail->next=nw;
+	nw->prev=tail;
+	tail=nw;
+	tail->next=NULL;
+	puts("");
 }
 void search()
 {
 	NODE *temp;
 	int roll;
-	puts("enter the roll number: ");
+	puts("\nenter the roll number: ");
 	scanf("%d",&roll);
-	for(temp=head;temp->st_rec.roll!=roll;temp=temp->next)
+	for(temp=head;(temp->st_rec.roll!=roll)&&temp!=NULL;temp=temp->next)
 	continue;
-	puts("Record found. It's between: ");
-	disp_pn(temp);
+	if(temp!=NULL)
+    {
+        puts("Record found. It's between: ");
+        disp_pn(temp);
+    }
+    else
+        puts("Record not found!");
 }
 void delete_rec()
 {
@@ -62,6 +91,8 @@ void delete_rec()
 	for(temp=head;temp->st_rec.roll!=roll;temp=temp->next)
 	continue;
 	disp_pn(temp);
+	temp->prev->next=temp->next;
+	temp->next->prev=temp->prev;
 	temp->prev=NULL;
 	temp->next=NULL;
 	free(temp);
@@ -69,22 +100,33 @@ void delete_rec()
 int main()
 {
 	int n,choice;
+	char choice2;
 	puts("enter the number of students: ");
 	scanf("%d",&n);
-	puts("Enter the records: ");
+	puts("\nEnter the records: \n");
 	create_rec(n);
-	puts("enter your option:\n1. Add one more record\n2. Search for a record\n3. Delete a record");
-	scanf("%d",&choice);
-	switch(choice)
+	do
 	{
-		case 1:
-			create_rec(1);
-			break;
-		case 2:
-			search();
-			break;
-		case 3:
-			delete_rec();
-			break;
-	}
+		puts("enter your option:\n1. Add one more record\n2. Search for a record\n3. Delete a record\n4. exit");
+		scanf("%d",&choice);
+		switch(choice)
+		{
+			case 1:
+				insert_rec();//inserting one data at a time
+				break;
+			case 2:
+				search();
+				break;
+			case 3:
+				delete_rec();
+				break;
+			case 4:
+				exit(0);
+		}
+		puts("\ncontinue? Y or N?");
+		fflush(stdin);
+		scanf("%c",&choice2);
+	}while(choice2=='Y'||choice2=='y');
+	return 0;
 }
+
